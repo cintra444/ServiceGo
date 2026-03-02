7ad7e10914817614b80e994288beac3a9275c799
Eber Cintra <organico_51@hotmail.com>
2026-03-01 22:28:47 -0300

    primeiro comit - inicializando projeto

---
+++ src/test/java/com/ServiceGo/ServiceGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ServiceGo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
